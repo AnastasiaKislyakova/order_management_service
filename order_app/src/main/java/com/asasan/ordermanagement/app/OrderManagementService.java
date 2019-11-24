d978c2fb038fb106d7e6df30ffda047b2b8ff0a3
AnastasiaKislyakova <anastasia.kislyakova@gmail.com>
2019-11-24 13:51:47 +0300

    added eureka client

--- order_app/src/main/java/com/asasan/ordermanagement/app/OrderManagementService.java
+++ order_app/src/main/java/com/asasan/ordermanagement/app/OrderManagementService.java
@@ -2,8 +2,10 @@ package com.asasan.ordermanagement.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderManagementService {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementService.class, args);
